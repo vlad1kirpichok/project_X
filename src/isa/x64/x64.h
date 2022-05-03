ae1b36325ba7dcad47949f64e0ff49b577b38800
Andrei Tatarnikov <andrewt0301@gmail.com>
2022-05-03 15:59:11 +0300

    up
    Signed-off-by: Andrei Tatarnikov <andrewt0301@gmail.com>

---
+++ src/isa/x64/x64.h
@@ -0,0 +1,31 @@
+
+#ifndef X_X64_H
+#define X_X64_H
+
+namespace x64 {
+
+/**
+ * Constants for x64 general purpose registers.
+ */
+Reg {
+  rax
+  rcx
+  rdx
+  rbx
+  rsi
+  rdi
+  rsp
+  rbp
+  r8
+  r9
+  r10
+  r11
+  r12
+  r13
+  r14
+  r15
+};
+
+} // namespace x64
+
+#endif //X_X64_H
