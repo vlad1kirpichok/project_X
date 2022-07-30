daf4d8046b96e2042114d88aec5d68fb27aab2fc
andrewt0301 <andrewt0301@gmail.com>
2022-07-30 16:46:03 +0300

    Test code is updated
    Signed-off-by: andrewt0301 <andrewt0301@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include "inject/inject.h"
+#include "isa/utils.h"
 #include "isa/aarch64/aarch64.h"
 #include "isa/riscv64/riscv64.h"
 #include "isa/x64/x64.h"
@@ -16,17 +17,17 @@ struct Program {
   mov<x1, x2> i1;
   mov<w1, w4> i2;
   b_<eq, 4>   i3;
-  bl<4>        i4;
+  bl<4>       i4;
   ret<>       i5;
 };
 
 void test() {
-    std::cout << std::hex << mov<x1, x2>{} << std::endl;
-    std::cout << std::hex << mov<x1, x2>{} << std::endl;
-    std::cout << std::hex << bl<4>{} << std::endl;
+  std::cout << std::hex << mov<x1, x2>{} << std::endl;
+  std::cout << std::hex << mov<x1, x2>{} << std::endl;
+  std::cout << std::hex << bl<4>{} << std::endl;
 }
 
-}
+} // namespace aarch64
 
 namespace riscv64 {
 /**
@@ -36,23 +37,39 @@ struct Program {
   addi<t0, zero, 1> i1;
   add<t1, zero, t0> i2;
 };
-}
+} // namespace riscv64
 
 namespace x64 {
 /**
  * x64 program to injected.
  */
 struct Program {
-  // TODO
+  pushq<rax> i0;
+  inc<rax> i01;
+  dec<rax> i02;
+  popq<rax> i1;
+  ret i2;
 };
+
+void test() {
+  print_bytes(pushq<rax>{});
+  print_bytes(inc<rax>{});
+  print_bytes(dec<rax>{});
+  print_bytes(popq<rax>{});
 }
+} // namespace x64
 
 int main(int argc, char* argv[]) {
+  x64::test();
+  x64::EmptyFunction()();
+
   aarch64::test();
   xinject::save("aarch64_test.bin", aarch64::Program{});
   xinject::save("riscv64_test.bin", riscv64::Program{});
   xinject::save("x64_test.bin",     x64::Program{});
 
+  riscv64::Function<8, 10> proga{puts, "hello"};
+  std::cout << sizeof(proga) << std::endl;
   std::cout << "This is a test" << std::endl;
   if (argc < 2) {
     std::cout << "File to be patched is not specified!" << std::endl;
@@ -60,6 +77,5 @@ int main(int argc, char* argv[]) {
   }
   int x = 0;
   xinject::inject(argv[1], x);
-
   return 0;
 }
